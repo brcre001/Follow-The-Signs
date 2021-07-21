f2499e99562ad88f5bd17655c65ba666b9d3b14c
brcre001 <brandon.f.creed@gmail.com>
2021-07-21 22:12:48 +0000

    completed the navbar styling

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,14 +3,36 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				<i className="fas fa-american-sign-language-interpreting" /> Follow The Signs
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav ml-auto">
+					<a className="text-white nav-link" href="#">
+						News
+					</a>
+					<a className="text-white nav-link" href="#">
+						Resources
+					</a>
+					<a className="text-white nav-link" href="#">
+						Total Users Online
+					</a>
+					<a className="text-white nav-link" href="#">
+						Your Connections Online
+					</a>
+					<button className="btn btn-primary">Sign Up</button>
+				</div>
 			</div>
 		</nav>
 	);
