0d6801542b1ff3fbb3718b1e44ae9d59bcb2c6f7
Allen St Fort <allenstfort1@gmail.com>
2021-09-05 17:28:34 +0000

    Added more features to the Modal in the discussion page button. Tooltip and username image

--- src/front/js/pages/discussions.js
+++ src/front/js/pages/discussions.js
@@ -1,11 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import FormControl from "react-bootstrap/FormControl";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 import "../../styles/discussions.scss";
 import { propTypes } from "react-bootstrap/esm/Image";
 
@@ -30,10 +31,16 @@ export const Discussions = () => {
 					</Form>
 				</div>
 			</div>
-			<Button className="ml-6" variant="primary" onClick={handleShow}>
-				Create <i className="fas fa-plus"></i>
-			</Button>
 
+			{/* THIS IS THE CREATE BUTTON */}
+			<div className="discussion-creation">
+				<Button className="ml-5 mb-2" variant="primary" onClick={handleShow}>
+					Create <i className="fas fa-plus"></i>
+				</Button>
+				<h2 className="discussion-article text-center">Discussions & Articles</h2>
+			</div>
+
+			{/* THIS IS THE MODEL */}
 			<Modal
 				aria-labelledby="contained-modal-title-vcenter"
 				centered
@@ -43,15 +50,38 @@ export const Discussions = () => {
 				keyboard={false}>
 				<Modal.Header closeButton>
 					<Modal.Title id="contained-modal-title-vcenter">
-						<h2 className="display-5 text-center">Create a discussion</h2>
+						<div className="">
+							<div>
+								<h2 className="display-5 text-center">Create a discussion</h2>
+							</div>
+							<div>
+								<i className="fas fa-user-circle fa-lg"></i>
+								<span className="font-weight-lighter pl-2">Username</span>{" "}
+								{/*This is for the username while change when login */}
+							</div>
+						</div>
 					</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<label className="lead" htmlFor="exampleFormControlTextarea1">
+					{/* <label className="lead" htmlFor="exampleFormControlTextarea1">
 						Write something you want to discuss
-					</label>
+					</label> */}
+					<div>
+						<textarea
+							className="form-control border-0"
+							id="exampleFormControlTextarea1"
+							placeholder="What's on your mind?"
+							rows="3"></textarea>
 
-					<textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+						<div className="d-flex border-top">
+							<p className="lead">Add to Your Post</p>
+							<OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Photo/Video</Tooltip>}>
+								<Button className="ml-auto mt-2">
+									<i className="fas fa-images"></i>
+								</Button>
+							</OverlayTrigger>
+						</div>
+					</div>
 				</Modal.Body>
 				<Modal.Footer>
 					<Button variant="secondary" onClick={handleClose}>
@@ -63,7 +93,7 @@ export const Discussions = () => {
 
 			{cardLoop.map(index => (
 				<div className="row justify-content-center p-1" key={index}>
-					<Card className="col-6 mt-5">
+					<Card className="col-6 mt-5" style={{ width: "18rem" }}>
 						<Card.Img
 							rounded
 							className="p-3"
