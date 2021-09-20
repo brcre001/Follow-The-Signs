d94ea35b41666e2cd3ee0e53da58288d157e8e6a
WillCJR <wcranmer@gmail.com>
2021-09-20 19:39:43 +0000

    Second news scrape working

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,12 +1,15 @@
 import click
 from flask import Flask
 from api.news_scrape import dailyMothNewsURL_news_scrape 
+from api.news_scrape import deafnewstoday_news_scrape 
+
 
 def initialize_commands(app): 
     @app.cli.command("scrape")
     def scrape():
         print("This is the commands dot py")
         dailyMothNewsURL_news_scrape()
+        deafnewstoday_news_scrape()
     # def scrape(site): if you want to use sites
 
 
